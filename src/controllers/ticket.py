95caccde4b08e0aa2c8e58edb8f2fae33410de2b
Jpifer13 <jakepifer3@gmail.com>
2024-02-16 10:30:15 -0800

    Refactor ticket generation and retrieval endpoints

--- src/controllers/ticket.py
+++ src/controllers/ticket.py
@@ -20,27 +20,32 @@ granted_user = token_authentication.require_user_with_permission(
 
 @router.post("/file/{file_name}/tickets")
 @authorized_api_handler()
-async def get_file(
+async def invoke_ticket_generation(
     file_name: str,
     number_of_tickets: Optional[int] = 10,
     platform: Optional[PlatformEnum] = PlatformEnum.JIRA,
     user: Dict = Depends(granted_user),
 ) -> TicketGenerationSchema:
-    """Get file from S3"""
+    """
+    This endpoint is for generating tickets from a transcript. It invokes a long
+    running lambda function to generate the tickets and returns the datetime when the
+    lambda was invoked. The tickets can be retrieved using the datetime returned.
+
+    Args:
+        file_name (str): The name of the file to generate tickets from.
+        number_of_tickets (Optional[int], optional): The number of tickets to generate. Defaults to 10.
+        platform (Optional[PlatformEnum], optional): The platform to generate the tickets for. Defaults to PlatformEnum.JIRA.
+        user (Dict, optional): The user making the request. Defaults to Depends(granted_user).
+
+    Returns:
+        TicketGenerationSchema: The datetime when the lambda was invoked.
+    """
     ticket_generation_datetime: str = await invoke_ticket_generation_lambda(
         document_id=file_name,
         user_id=user.get("sub"),
         number_of_tickets=number_of_tickets,
         platform=platform,
     )
-
-    # logger.info(f"Getting file from S3: {file_name}")
-    # _: str = user.get("sub").split("|")[1]
-    # resp: str = download_file_from_s3(file_name)
-
-    # logger.info(f"Generating tickets from transcript: {file_name}")
-    # tickets: dict = generate_tickets(prompt=resp, number_of_tickets=number_of_tickets, platform=platform)
-    # logger.info(f"Tickets generated from transcript: {tickets}")
 
     return {"ticket_generation_datetime": ticket_generation_datetime}
 
@@ -52,7 +57,18 @@ async def get_tickets_by_generation_time(
     generation_datetime: str,
     _: Dict = Depends(granted_user),
 ) -> TicketList:
-    """Get all tickets"""
+    """
+    This endpoint is for retrieving tickets generated from a transcript using the
+    datetime when the lambda was invoked.
+
+    Args:
+        file_name (str): The name of the file to retrieve tickets from.
+        generation_datetime (str): The datetime when the lambda was invoked.
+        _: Dict, optional): The user making the request. Defaults to Depends(granted_user).
+
+    Returns:
+        TicketList: The list of tickets generated from the transcript.
+    """
     ticket: Optional[TicketModel] = await get_tickets(
         document_id=file_name, generation_datetime=generation_datetime
     )
--- ui/src/views/UploadTranscript.jsx
+++ ui/src/views/UploadTranscript.jsx
@@ -120,6 +120,10 @@ export default function UploadTranscript() {
         }
     };
 
+    const saveTicket = async (fileName, subject, body, estimationpoints) => {
+
+    };
+
     const uploadButton = (
         <label
             htmlFor="upload"
@@ -222,6 +226,14 @@ export default function UploadTranscript() {
                             </td>
                             <td className="py-4 text-sm text-gray-500">
                                 {estimationpoints}
+                            </td>
+                            <td>
+                                <button
+                                    className="relative cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                    onClick={() => saveTicket({subject, body, estimationpoints})}
+                                >
+                                    Save Ticket
+                                </button>
                             </td>
                         </tr>
                     ))}
