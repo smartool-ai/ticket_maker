54e815ad66c0f124a089464e7d09ea4e2437b840
Renerose Dimatulac <rsdimatulac@gmail.com>
2024-02-19 17:04:29 -0800

    Update logo

--- ui/src/components/Logo.jsx
+++ ui/src/components/Logo.jsx
@@ -2,15 +2,29 @@ import logo from "../static/logo.svg";
 import logoInverted from "../static/logo_inverted.svg";
 
 export default function Logo({ inverted, size = "lg" }) {
-  const sizes = {
-    "sm": "h-12 w-12",
-    "md": "h-20 w-20",
-    "lg": "h-28 w-28",
-  }
+  const logoSizes = {
+    sm: "h-12 w-12 z-10",
+    md: "h-20 w-20 z-10",
+    lg: "h-28 w-28 z-10",
+  };
 
-  return <img
-    className={sizes[size]}
-    src={inverted ? logoInverted : logo}
-    alt="Transcriber"
-  />
+  const logoBackgroundSizes = {
+    sm: "h-6 w-6",
+    md: "h-10 w-10",
+    lg: "h-16 w-16",
+    default: "bg-white absolute z-1",
+  };
+
+  const combineClassNames = (id, classNameObject) => [classNameObject[id], classNameObject.default].join(" ");
+
+  return (
+    <div className="relative flex items-center justify-center">
+      <div className={combineClassNames(size, logoBackgroundSizes)}></div>
+      <img
+        className={logoSizes[size]}
+        src={inverted ? logoInverted : logo}
+        alt="Transcriber"
+      />
+    </div>
+  )
 }
