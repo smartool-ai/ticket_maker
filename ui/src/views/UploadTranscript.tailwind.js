a23b7237adebe81b924a545f7711631a14e97cad
Renerose Dimatulac <rsdimatulac@gmail.com>
2024-02-19 19:48:02 -0800

    Update UploadTranscript, refactor, clean up. Move all css in its own tailwind file.

---
+++ ui/src/views/UploadTranscript.tailwind.js
@@ -0,0 +1,19 @@
+export const uploadButton_tw = "relative cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4654A3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600";
+
+export const transcriptContainer_tw = "flex flex-col gap-6";
+
+export const bucketTableContainer_tw = "w-full divide-y divide-gray-300";
+
+export const tableHeader_tw = "py-3.5 text-left text-sm font-semibold text-white";
+
+export const tableBodyContainer_tw = "divide-y divide-gray-200";
+
+export const generateTicketsButton_tw = "relative cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4654A3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600";
+
+export const regenerateTicketsButton_tw = "relative cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4654A3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600";
+
+export const saveTicketButton_tw = "relative cursor-pointer flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4654A3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600";
+
+export const fileTableContainer_tw = "w-full divide-y divide-gray-300";
+
+export const fileTableUrl_tw = "text-blue-600 hover:text-[#4654A3] text-xs";
