1ab452ea26965d0ac4fa6fe72f4128c5fd555f0a
Renerose Dimatulac <rsdimatulac@gmail.com>
2024-03-17 20:04:20 -0700

    Update toast style

--- ui/src/components/Toast.jsx
+++ ui/src/components/Toast.jsx
@@ -5,16 +5,23 @@ import {
     InformationCircleIcon,
     ExclamationTriangleIcon,
     ExclamationCircleIcon,
-    XCircleIcon
+    XMarkIcon
 } from '@heroicons/react/20/solid';
 
 
 export default function Toast({ label, onClose, type = "info" }) {
     const alertType = {
-        error: <ExclamationCircleIcon className="text-red-600"/>,
-        info: <InformationCircleIcon className="text-blue-400"/>,
-        success: <CheckCircleIcon className="text-green-600"/>,
-        warning: <ExclamationTriangleIcon className="text-orange-400"/>,
+        error: <ExclamationCircleIcon />,
+        info: <InformationCircleIcon />,
+        success: <CheckCircleIcon />,
+        warning: <ExclamationTriangleIcon />,
+    };
+
+    const bgColor = {
+        error: "bg-red-600",
+        info: "bg-blue-400",
+        success: "bg-green-600",
+        warning: "bg-orange-400",
     };
 
     return (
@@ -25,20 +32,17 @@ export default function Toast({ label, onClose, type = "info" }) {
                 enter="transform ease-out duration-300 transition"
                 enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
                 enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-                leave="transition-opacity duration-75"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
                 show
                 >
-                <div className="bg-gray-100 rounded-md p-4 flex gap-2 max-w-fit absolute z-10 right-0">
-                    <div className="h-5 w-5" aria-hidden="true">
+                <div className={["bg-gray-100 rounded-md p-4 flex gap-2 max-w-fit absolute z-10 right-0", bgColor[type]].join(" ")}>
+                    <div className="h-5 w-5 text-white" aria-hidden="true">
                         {alertType[type]}
                     </div>
-                    <div className="text-sm text-gray-700">
+                    <p className="text-sm text-white font-semibold">
                         {label}
-                    </div>
-                    <div className="h-5 w-5 text-gray-700 hover:text-[#4654A3] ml-1" onClick={onClose}>
-                        <XCircleIcon />
+                    </p>
+                    <div className="h-5 w-5 text-white font-bold hover:border-2 hover:rounded-md ml-1" onClick={onClose}>
+                        <XMarkIcon />
                     </div>
                 </div>
             </Transition>
