0b3984706beb5504c02a601f0fa146aeedd9374a
Jpifer13 <jakepifer3@gmail.com>
2024-03-19 22:05:07 -0700

    commented out stripe page

--- ui/src/views/UserSettings.jsx
+++ ui/src/views/UserSettings.jsx
@@ -3,16 +3,16 @@ import Payment from "../components/Payment";
 import { Elements } from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 
-
-const stripePromise = loadStripe(__REACT_APP_STRIPE_PUBLIC_KEY__);
+// console.log('REACT_APP_STRIPE_PUBLIC_KEY:', __REACT_APP_STRIPE_PUBLIC_KEY__);
+// const stripePromise = loadStripe(__REACT_APP_STRIPE_PUBLIC_KEY__);
 
 export default function UserSettings() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const stripeOptions = {
-        clientSecret: __REACT_APP_STRIPE_CLIENT_SECRET__,
-    }
+    // const stripeOptions = {
+    //     clientSecret: __REACT_APP_STRIPE_CLIENT_SECRET__,
+    // }
 
     const handleNameChange = (e) => {
         setName(e.target.value);
@@ -33,7 +33,7 @@ export default function UserSettings() {
 
     return (
 
-        <Elements stripe={stripePromise} options={stripeOptions}>
+        // <Elements stripe={stripePromise} options={stripeOptions}>
         <div>
             <h1>User Settings</h1>
             <form onSubmit={handleSubmit}>
@@ -54,8 +54,8 @@ export default function UserSettings() {
                 <br />
                 <button type="submit">Save</button>
             </form>
-            <Payment />
+            {/* <Payment /> */}
         </div>
-        </Elements>
+        // </Elements>
     );
 };
